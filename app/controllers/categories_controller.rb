495b14d9ce8380fe941a1aa50197dbe7f3b1787a
Nick Riebeek <nick.riebeek@gmail.com>
2012-04-14 21:46:59 -0400

    bundle update

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
   respond_to :html, :json
   
   def index
-    @category = Category.new
+    @category = Category.newa
     @categories = current_user.categories
   end
   
