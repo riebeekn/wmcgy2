623c5495de9b3a22bc73a2ebed28890e1ddfaa01
Nick Riebeek <nick.riebeek@gmail.com>
2012-07-06 15:43:56 -0400

    fixed account modification for oauth users

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ end
 
 module Wmcgy
   class Application < Rails::Application
+    # Load heroku vars from local file
+    heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
+    load(heroku_env) if Rails.env.development? && File.exists?(heroku_env)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,159 +5,191 @@ describe "Users" do
   subject { page }
   
   describe "edit account" do
-    let(:user) { FactoryGirl.create(:user, active: true) }
-    before do 
-      sign_in user 
-      visit account_path
-    end
-    
-    describe "items that should be present on page" do
-      it { should have_selector('h1', text: 'Account Settings') }
-      # change email section
-      it { should have_selector('header', text: 'Change email') }
-      it { should have_field('Email', with: user.email) }
-      it { should have_button('Update email') }
-      #change password section
-      it { should have_selector('header', text: 'Change password') }
-      it { should have_field('Old password') }
-      it { should have_field('New password') }
-      it { should have_field('Confirm new password') }
-      it { should have_button('Update password') }
-      it { should have_link('Forgot password') }
-      #delete account section
-      it { should have_selector('header', text: 'Delete account') }
-      it { should have_content('Account deletion cannot be undone, please be certain.') }
-      it { should have_button('Delete account') }
-      
-      context "mtd / ytd widget" do
-       it_behaves_like 'mtd / ytd widget'
-      end
-    end
-    
-    describe "change email" do
-      
-      context "valid email" do
-        before do
-          fill_in "Email", with: "a.new.email@example.com"
-          click_button "Update email"
-        end
-        
-        it "should update the user's email" do  
-          User.find(user).email.should eq "a.new.email@example.com"
-        end
-        
-        it "should display a message that the email was updated" do
-          page.should have_content "Email updated"
-        end
+    context "oauth authentication account" do
+      let(:user) { FactoryGirl.create(:user, active: true, provider: 'some oauth provider') }
+      before do 
+        sign_in user 
+        visit account_path
       end
       
-      context "invalid email" do
-        it "should not update the user's email when email has already been taken" do
-          FactoryGirl.create(:user, email: "a.popular.email@example.com")
-          fill_in "Email", with: "a.popular.email@example.com"
-          click_button "Update email"
-          page.should_not have_content "Signed in as a.popular.email@example.com"
-          page.should have_content "has already been taken"
-        end
+      describe "items that should be present on page" do
+        it { should have_selector('h1', text: 'Account Settings') }
+        # change email section
+        it { should_not have_selector('header', text: 'Change email') }
+        it { should_not have_field('Email', with: user.email) }
+        it { should_not have_button('Update email') }
+        #change password section
+        it { should_not have_selector('header', text: 'Change password') }
+        it { should_not have_field('Old password') }
+        it { should_not have_field('New password') }
+        it { should_not have_field('Confirm new password') }
+        it { should_not have_button('Update password') }
+        it { should_not have_link('Forgot password') }
+        #delete account section
+        it { should have_selector('header', text: 'Delete account') }
+        it { should have_content('Account deletion cannot be undone, please be certain.') }
+        it { should have_button('Delete account') }
       
-        it "should not update the user's email if blank" do
-          fill_in "Email", with: "    "
-          click_button "Update email"
-          page.should have_content "can't be blank"
-          page.should_not have_content "Signed in as    "
-          User.find(user.id).email.should_not eq ""
-        end
-        
-        it "should not update the user's email if format is invalid" do
-          fill_in "Email", with: "a bad email"
-          click_button "Update email"
-          page.should have_content "is invalid"
-          page.should_not have_content "Signed in as a bad email"
-          User.find(user.id).email.should_not eq "a bad email"
+        context "mtd / ytd widget" do
+          it_behaves_like 'mtd / ytd widget'
         end
       end
     end
     
-    describe "change password" do
-      
-      context "valid information" do
-        before do
-          fill_in "Old password", with: "foobar"
-          fill_in "New password", with: "foobar2"
-          fill_in "Confirm new password", with: "foobar2"
-        end
-        
-        it "should change the password digest" do
-          original_digest = User.find(user).password_digest
-          click_button "Update password"
-          User.find(user).password_digest.should_not eq original_digest
-        end
-        
-        it "should display a message that the password was updated" do
-          click_button "Update password"
-          page.should have_content "Password updated"
-        end
+    context "custom authentication account" do
+      let(:user) { FactoryGirl.create(:user, active: true) }
+      before do 
+        sign_in user 
+        visit account_path
       end
       
-      context "invalid information" do
-        before do
-          @original_digest = User.find(user).password_digest
+      describe "items that should be present on page" do
+        it { should have_selector('h1', text: 'Account Settings') }
+        # change email section
+        it { should have_selector('header', text: 'Change email') }
+        it { should have_field('Email', with: user.email) }
+        it { should have_button('Update email') }
+        #change password section
+        it { should have_selector('header', text: 'Change password') }
+        it { should have_field('Old password') }
+        it { should have_field('New password') }
+        it { should have_field('Confirm new password') }
+        it { should have_button('Update password') }
+        it { should have_link('Forgot password') }
+        #delete account section
+        it { should have_selector('header', text: 'Delete account') }
+        it { should have_content('Account deletion cannot be undone, please be certain.') }
+        it { should have_button('Delete account') }
+      
+        context "mtd / ytd widget" do
+         it_behaves_like 'mtd / ytd widget'
         end
-        
-        context "incorrect old password" do
+      end
+    
+      describe "change email" do
+      
+        context "valid email" do
           before do
-            fill_in "Old password", with: "barbaz"
+            fill_in "Email", with: "a.new.email@example.com"
+            click_button "Update email"
+          end
+        
+          it "should update the user's email" do  
+            User.find(user).email.should eq "a.new.email@example.com"
+          end
+        
+          it "should display a message that the email was updated" do
+            page.should have_content "Email updated"
+          end
+        end
+      
+        context "invalid email" do
+          it "should not update the user's email when email has already been taken" do
+            FactoryGirl.create(:user, email: "a.popular.email@example.com")
+            fill_in "Email", with: "a.popular.email@example.com"
+            click_button "Update email"
+            page.should_not have_content "Signed in as a.popular.email@example.com"
+            page.should have_content "has already been taken"
+          end
+      
+          it "should not update the user's email if blank" do
+            fill_in "Email", with: "    "
+            click_button "Update email"
+            page.should have_content "can't be blank"
+            page.should_not have_content "Signed in as    "
+            User.find(user.id).email.should_not eq ""
+          end
+        
+          it "should not update the user's email if format is invalid" do
+            fill_in "Email", with: "a bad email"
+            click_button "Update email"
+            page.should have_content "is invalid"
+            page.should_not have_content "Signed in as a bad email"
+            User.find(user.id).email.should_not eq "a bad email"
+          end
+        end
+      end
+    
+      describe "change password" do
+      
+        context "valid information" do
+          before do
+            fill_in "Old password", with: "foobar"
             fill_in "New password", with: "foobar2"
             fill_in "Confirm new password", with: "foobar2"
-            click_button "Update password"
-          end
-          
-          it "should not update password digest" do  
-            User.find(user).password_digest.should eq @original_digest
           end
         
-          it "should display a message" do
-            page.should have_content "incorrect old password"
+          it "should change the password digest" do
+            original_digest = User.find(user).password_digest
+            click_button "Update password"
+            User.find(user).password_digest.should_not eq original_digest
+          end
+        
+          it "should display a message that the password was updated" do
+            click_button "Update password"
+            page.should have_content "Password updated"
           end
         end
-        
-        context "invalid new password" do
+      
+        context "invalid information" do
           before do
-            fill_in "Old password", with: "foobar"
-            fill_in "New password", with: "a"
-            fill_in "Confirm new password", with: "a"
-            click_button "Update password"
+            @original_digest = User.find(user).password_digest
           end
-          
-          it "should not update password digest" do  
-            User.find(user).password_digest.should eq @original_digest
-          end
-          
-          it "should display a message" do
-            page.should have_content "is too short (minimum is 6 characters)"
-          end
-        end
         
-        context "mis-matching new password and new password confirmation" do
-          before do
-            fill_in "Old password", with: "foobar"
-            fill_in "New password", with: "barbaz"
-            fill_in "Confirm new password", with: "barbax"
-            click_button "Update password"
-          end
+          context "incorrect old password" do
+            before do
+              fill_in "Old password", with: "barbaz"
+              fill_in "New password", with: "foobar2"
+              fill_in "Confirm new password", with: "foobar2"
+              click_button "Update password"
+            end
           
-          it "should not update password digest" do  
-            User.find(user).password_digest.should eq @original_digest
+            it "should not update password digest" do  
+              User.find(user).password_digest.should eq @original_digest
+            end
+        
+            it "should display a message" do
+              page.should have_content "incorrect old password"
+            end
           end
+        
+          context "invalid new password" do
+            before do
+              fill_in "Old password", with: "foobar"
+              fill_in "New password", with: "a"
+              fill_in "Confirm new password", with: "a"
+              click_button "Update password"
+            end
           
-          it "should display a message" do
-            page.should have_content "doesn't match confirmation"
+            it "should not update password digest" do  
+              User.find(user).password_digest.should eq @original_digest
+            end
+          
+            it "should display a message" do
+              page.should have_content "is too short (minimum is 6 characters)"
+            end
+          end
+        
+          context "mis-matching new password and new password confirmation" do
+            before do
+              fill_in "Old password", with: "foobar"
+              fill_in "New password", with: "barbaz"
+              fill_in "Confirm new password", with: "barbax"
+              click_button "Update password"
+            end
+          
+            it "should not update password digest" do  
+              User.find(user).password_digest.should eq @original_digest
+            end
+          
+            it "should display a message" do
+              page.should have_content "doesn't match confirmation"
+            end
           end
         end
       end
-    end
     
-    describe "delete user" do
+      describe "delete user" do
       before do
         click_button "Delete account"
       end
@@ -174,6 +206,7 @@ describe "Users" do
       it "should display an account deleted message" do
         page.should have_content("Account closed")
       end
+    end
     end
   end
     
