9417820e6ec087bfa616d6e94dc4257bb62ff690
Nick Riebeek <nickriebeek@Nicks-MacBook-Pro-3.local>
2016-04-16 14:33:50 -0400

    updated ssl key

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,10 @@ class StaticPagesController < ApplicationController
   skip_before_filter :signed_in_user
   
   def letsencrypt
-    # use your code here, not mine
-    render text: "06Z4rK6JkGYENkhInyhUpkWhew7V53gAWj6-rTSt3l4.ElwIer_jaW8HCKuGoiS_jg9BQ-vXBrFsbbKFYlXCG78"
+    if request.protocol.include? "www."
+    	render text: "p8dPCmmZZ1BxIN8ckXRgpmpBIdwPy6d3yVtFdn4XPNg.ElwIer_jaW8HCKuGoiS_jg9BQ-vXBrFsbbKFYlXCG78"
+    else
+    	render text: "06Z4rK6JkGYENkhInyhUpkWhew7V53gAWj6-rTSt3l4.ElwIer_jaW8HCKuGoiS_jg9BQ-vXBrFsbbKFYlXCG78"
+	end
   end
 end
