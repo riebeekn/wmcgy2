67d817aab5387d262c5a5536c15709df943dff2b
Nick Riebeek <nickriebeek@Nicks-MacBook-Pro-3.local>
2016-07-15 16:42:52 -0400

    updated ssl cert

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,6 @@ class StaticPagesController < ApplicationController
   skip_before_filter :signed_in_user
   
   def letsencrypt
-    render text: "s0ZnQ5gItHwnj89ztPim28NwlXgj3emVmZZ0W0o_N7Y.ElwIer_jaW8HCKuGoiS_jg9BQ-vXBrFsbbKFYlXCG78"
+    render text: "y38eP6vVEkliINf_hYy68vuTFeYwhCyIKjuRU8LjX70.ElwIer_jaW8HCKuGoiS_jg9BQ-vXBrFsbbKFYlXCG78"
   end
 end
