f552f44c61bd138179a154d5977ea2d68d367119
Nick Riebeek <nickriebeek@MacBook-Pro.local>
2016-10-12 17:39:02 -0400

    updated cert

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,6 @@ class StaticPagesController < ApplicationController
   skip_before_filter :signed_in_user
   
   def letsencrypt
-    render text: "y38eP6vVEkliINf_hYy68vuTFeYwhCyIKjuRU8LjX70.ElwIer_jaW8HCKuGoiS_jg9BQ-vXBrFsbbKFYlXCG78"
+    render text: "OkeBnoqeTJ63te5FrioJWtL_EFRcNA_deC7Qjl1ZBUY.ElwIer_jaW8HCKuGoiS_jg9BQ-vXBrFsbbKFYlXCG78"
   end
 end
