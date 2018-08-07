f9a0858a8f7f6c35c5f1d9609be6b31ee60eae26
Nick Riebeek <nick.riebeek@gmail.com>
2018-08-07 17:11:50 -0400

    updated cert

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,6 @@ class StaticPagesController < ApplicationController
   skip_before_filter :signed_in_user
 
   def letsencrypt
-    render text: "IqFDFYj-ITCyQNeUWOQyEN8KwhNfB6C0IOsxzsIgmxI.ElwIer_jaW8HCKuGoiS_jg9BQ-vXBrFsbbKFYlXCG78"
+    render text: "LDBbulJ3Q4jDvY9j47Ab3rV8Cg3Yc06Kmdx9OT7HDig.ElwIer_jaW8HCKuGoiS_jg9BQ-vXBrFsbbKFYlXCG78"
   end
 end
